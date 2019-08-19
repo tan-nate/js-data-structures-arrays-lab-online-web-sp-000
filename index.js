9c36ee642dba93fe7f4a660b86a9e97e2445c03a
Nate Tan <nktan93@gmail.com>
2019-08-19 21:28:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ function destructivelyAppendDriver(driver) {
 function destructivelyPrependDriver(driver) {
   drivers.unshift(driver);
 }
+
+function destructivelyRemoveLastDriver(driver) {
+  drivers.pop(driver);
+}
+
+function destructivelyRemoveFirstDriver(driver) {
+  drivers.shift(driver);
+}
+
+function appendDriver(driver) {
+  return [...drivers, driver];
+}
